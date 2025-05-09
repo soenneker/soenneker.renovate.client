71a03457283c8ee5c4b349d09ec40be85f530800
Jake Soenneker <jake@soenneker.com>
2025-05-09 12:15:20 -0500

    namespace

--- src/RenovateClient.cs
+++ src/RenovateClient.cs
@@ -3,17 +3,17 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System;
 using System.Threading;
+using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.Utils.HttpClientCache.Abstract;
-using Soenneker.Utils.HttpClientCache.Dtos;
 
 namespace Soenneker.Renovate.Client;
 
 /// <inheritdoc cref="IRenovateClient"/>
-public class RenovateClient: IRenovateClient
+public sealed class RenovateClient : IRenovateClient
 {
     private readonly IHttpClientCache _httpClientCache;
 
-    private readonly HttpClientOptions _options = new() { UseCookieContainer = true };
+    private const string _clientId = nameof(RenovateClient);
 
     public RenovateClient(IHttpClientCache httpClientCache)
     {
@@ -22,20 +22,21 @@ public class RenovateClient: IRenovateClient
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
-        return _httpClientCache.Get(nameof(RenovateClient), _options, cancellationToken: cancellationToken);
+        return _httpClientCache.Get(_clientId, () => new HttpClientOptions
+        {
+            UseCookieContainer = true
+        }, cancellationToken);
     }
 
     public void Dispose()
     {
         GC.SuppressFinalize(this);
-
-        _httpClientCache.RemoveSync(nameof(RenovateClient));
+        _httpClientCache.RemoveSync(_clientId);
     }
 
     public ValueTask DisposeAsync()
     {
         GC.SuppressFinalize(this);
-
-        return _httpClientCache.Remove(nameof(RenovateClient));
+        return _httpClientCache.Remove(_clientId);
     }
 }
